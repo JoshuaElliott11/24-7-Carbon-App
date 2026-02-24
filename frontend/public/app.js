b97f6c051452886c8289e29f2463f44958bba5a7
Joshua Elliott <joshua@gridcog.com>
2026-02-24 11:13:02 +0000

    Use same-origin API base for deployed UI

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,4 +1,4 @@
-const API_BASE = window.API_BASE || "http://localhost:8000";
+const API_BASE = window.API_BASE || window.location.origin;
 let lastResult = null;
 let resourceCounter = 0;
 let gridDefaults = {};
